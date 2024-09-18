af27c4967442ba725493a3135650b5e7a164d37c
Winston <winstonalfred48@gmail.com>
2024-09-18 12:25:16 +0700

    modified:   components/clientRow.tsx

--- components/clientRow.tsx
+++ components/clientRow.tsx
@@ -16,13 +16,13 @@ const ClientRow: React.FC<ClientRowProps> = ({ detail, index }) => {
   const getItemColor = (type: ActivityType): string => {
     switch(type) {
       case 'Loading':
-        return 'bg-blue-100 text-blue-800';
+        return 'bg-white text-black';
       case 'Unloading':
-        return 'bg-green-100 text-green-800';
+        return 'bg-white text-black';
       case 'Daily Activities':
-        return 'bg-purple-100 text-purple-800';
+        return 'bg-white text-black';
       default:
-        return 'bg-gray-100 text-gray-800';
+        return 'bg-white text-black';
     }
   };
 
